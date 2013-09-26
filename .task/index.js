edec9f9ea62e4a64d094021f89d2f840fb95589f
Juan Placencia <juan.placencia.512@gmail.com>
2013-09-26 15:32:39 -0500

    Add scaffold tasks

--- .task/index.js
+++ .task/index.js
@@ -1,19 +1,27 @@
 'use strict';
 
+var bootstrap = require('./tasks/modules/bootstrap');
 var bower = require('./tasks/bower');
 var build = require('./tasks/build');
 var help = require('./tasks/help');
+var scaffold = {
+  model:          require('./tasks/scaffolds/model'),
+  collection:     require('./tasks/scaffolds/collection'),
+  view:           require('./tasks/scaffolds/view'),
+  collectionView: require('./tasks/scaffolds/collection-view'),
+  controller:     require('./tasks/scaffolds/controller'),
+  test:           require('./tasks/scaffolds/test')
+};
 
 module.exports = {
-
   add: {
     bootstrap: {
       description:  'Add Bootstrap',
-      task:         bootstrap.add.bind(bootstrap),
+      task:         function(){ bootstrap.add() },
 
       js: {
-        description:  'Add Bootstrap JavaScript',
-        task:         bootstrap.js.add.bind(bootstrap.js)
+        description:  'Add Bootstrap JavaScript\n',
+        task:         function(){ bootstrap.js.add() }
       }
     }
   },
@@ -21,12 +29,66 @@ module.exports = {
   rem: {
     bootstrap: {
       description:  'Remove Bootstrap and any of the above Bootstrap-related',
-      task:         bootstrap.remove.bind(bootstrap),
+      task:         function(){ bootstrap.remove() },
 
       js: {
-        description:  'Remove Bootstrap JavaScript',
-        task:         bootstrap.js.remove.bind(bootstrap.js)
+        description:  'Remove Bootstrap JavaScript\n',
+        task:         function(){ bootstrap.js.remove() }
       }
+    }
+  },
+
+  gen: {
+    model: {
+      description:  'Generate a Chaplin Model',
+      task:         function(){ scaffold.model.generate() }
+    },
+    view: {
+      description:  'Generate a Chaplin View',
+      task:         function(){ scaffold.view.generate() }
+    },
+    collection: {
+      description:  'Generate a Chaplin Collection + Model',
+      task:         function(){ scaffold.collection.generate() },
+      view: {
+        description:  'Generate a Chaplin CollectionView + Item View',
+        task:         function(){ scaffold.collectionView.generate() }
+      }
+    },
+    controller: {
+      description:  'Generate a Chaplin Controller',
+      task:         function(){ scaffold.controller.generate() }
+    },
+    test: {
+      description:  'Generate a Mocha test file\n',
+      task:         function(){ scaffold.test.generate() }
+    }
+  },
+
+  del: {
+    model: {
+      description:  'Delete a Chaplin Model',
+      task:         function(){ scaffold.model.generate() }
+    },
+    view: {
+      description:  'Delete a Chaplin View',
+      task:         function(){ scaffold.view.generate() }
+    },
+    collection: {
+      description:  'Delete a Chaplin Collection + Model',
+      task:         function(){ scaffold.collection.generate() },
+      view: {
+        description:  'Delete a Chaplin CollectionView + Item View',
+        task:         function(){ scaffold.collectionView.generate() }
+      }
+    },
+    controller: {
+      description:  'Delete a Chaplin Controller',
+      task:         function(){ scaffold.controller.generate() }
+    },
+    test: {
+      description:  'Delete a Mocha test file\n',
+      task:         function(){ scaffold.test.generate() }
     }
   },
 
