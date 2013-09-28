7398a5030f42e01078254889506e23391c41e034
Juan Placencia <juan.placencia.512@gmail.com>
2013-09-27 19:28:35 -0500

    Expose version number for bower-json based tasks

--- .task/tasks/modules/rivets.js
+++ .task/tasks/modules/rivets.js
@@ -3,9 +3,13 @@
 var bower = require('../bower');
 var rivets = module.exports = Object.create(null);
 
+rivets.version = '~0.5.13'
+
 rivets.add = function() {
+  var self = this;
+
   bower.edit(function(json) {
-    json.dependencies.rivets = '~0.5.13';
+    json.dependencies.rivets = self.version;
     json.overrides.rivets = {
       main: 'dist/rivets.js'
     };
--- .task/tasks/modules/underscore-string.js
+++ .task/tasks/modules/underscore-string.js
@@ -3,9 +3,13 @@
 var bower = require('../bower');
 var _string = module.exports = Object.create(null);
 
+_string.version = '~2.3.3';
+
 _string.add = function() {
+  var self = this;
+
   bower.edit(function(json) {
-    json.dependencies['underscore.string'] = '~2.3.3';
+    json.dependencies['underscore.string'] = self.version;
     json.overrides['underscore.string'] = {
       dependencies: {
         lodash: '*'
