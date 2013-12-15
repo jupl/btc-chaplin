fd5365122eaa976e095332c4aa958a42f638f2e3
Juan Placencia <juan.placencia.512@gmail.com>
2013-12-14 21:42:04 -0600

    Undo brunch-config.coffee

--- brunch-config.js
+++ brunch-config.js
@@ -1,18 +1,26 @@
-setup = require('./setup')
+var setup = require('./setup');
 
-exports.config = setup
-  server:
+exports.config = setup({
+  server: {
     path: 'server'
+  },
 
-  files:
-    javascripts:
-      joinTo:
+  files: {
+    javascripts: {
+      joinTo: {
         'javascripts/app.js': /^app/,
         'javascripts/vendor.js': /^(vendor|bower_components)/
+      }
+    },
 
-    stylesheets:
-      joinTo:
+    stylesheets: {
+      joinTo: {
         'stylesheets/app.css': /^(app|vendor|bower_components)/
+      }
+    },
 
-    templates:
+    templates: {
       joinTo: 'javascripts/app.js'
+    }
+  }
+});
